2d1a51b6bbed4d2aa2d14e1ecaf6958ae14ed119
keijiek <ekoukai.ookuma@gmail.com>
2026-06-01 11:24:04 +0900

    setup.sh is updated.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -e package.json ]; then
+  echo "package.json already exists. Empty project is required to run this setup script."
+  exit 1
+fi
+
 printf '{}\n' > package.json
 npm i -D vite tailwindcss @tailwindcss/vite vite-plugin-handlebars fast-glob
 npm i alpinejs lucide swiper gsap
